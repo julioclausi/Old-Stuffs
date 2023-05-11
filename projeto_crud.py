02757e4884bb51bc71f970ad6ca0769a7588b1e8
Julio Clausi <julioclausi@gmail.com>
2023-05-11 00:31:01 -0300

    Adicionei a mensagem de voltar ao menu principal.

--- projeto_crud.py
+++ projeto_crud.py
@@ -22,10 +22,9 @@
 #•	Fim da aplicação. 
 #Observação: O sistema deve obrigatoriamente fechar sempre que o usuário selecionar a opção cadastral e a funcionalidade. As opções “Sair” e “Voltar ao menu principal” ficarão sem funcionalidade por enquanto.
 
-from os import close
-
 #Aqui estou criando a uma função que terá a utilidade apenas de imprimir o menu principal
 def imprimir_menu_principal():
+    """Imprime o Menu Principal"""
     print ('---- MENU PRINCIPAL ----')
     print ('(1) Gerenciar Estudantes.')
     print ('(2) Gerenciar Disciplinas.')
@@ -36,13 +35,14 @@ def imprimir_menu_principal():
 
 #Aqui estou criando a uma função que terá a utilidade apenas de imprimir o menu de operações
 def imprimir_menu_operacoes():
+    """Imprime o Menu de Operações"""
     print ('(1) Incluir')
     print ('(2) Listar')
     print ('(3) Atualizar')
     print ('(4) Excluir')
     print ('(9) Voltar ao menu principal.\n')
 
-#Aqui estou criando uma lista das categorias Estudantes, Disciplinas, Professores, Turmas e Matrículas
+#Aqui estou criando uma lista das categorias e das ações
 categorias = ['Estudantes','Disciplinas','Professores','Turmas','Matrículas']
 acoes = ['Incluir', 'Listar', 'Atualizar', 'Excluir', 'Voltar ao menu principal']
 
@@ -52,10 +52,9 @@ selecao_usuario = input ('Por favor, selecione uma opção: ') #solicitando para
 
 if selecao_usuario.isdigit():
     #Método isdigit() retorna True se o usuário digitou apenas número e False caso contrário
-
     if int(selecao_usuario) < 6 and int(selecao_usuario) > 0:
     #Neste if verifico se o usuário digitou 1, 2, 3, 4 ou 5
-        categoria_selecionada = categorias[int(selecao_usuario)-1].upper() #salvo na variável categoria_selecionada a opção da lista de categorias referente à seleção do usuário
+        categoria_selecionada = categorias[int(selecao_usuario)-1] #salvo na variável categoria_selecionada a opção da lista de categorias referente à seleção do usuário
         print (f'Você selecionou a opção de gerenciamento de {categoria_selecionada}\n') #um \n adicionou para ficar visualmente melhor
         print (f'---- [{categoria_selecionada}] Menu de Operações ----') 
         imprimir_menu_operacoes() #imprimindo o menu de operações
@@ -63,18 +62,18 @@ if selecao_usuario.isdigit():
         selecao_acao = input('Agora entre com a ação desejada: ')
         if selecao_acao.isdigit():
             if int(selecao_acao) <5 and int(selecao_acao) >0:
-                acao_selecionada = acoes[int(selecao_acao)-1].upper()
-                print (f'\nVocê selecionou a opção de [{acao_selecionada}] na categoria [{categoria_selecionada}]\n')
+                acao_selecionada = acoes[int(selecao_acao)-1]
+                print (f'Você selecionou a opção de [{acao_selecionada}] na categoria [{categoria_selecionada}]\n')
+            elif int(selecao_acao) == 9:
+                print ('Você selecionou a opção de voltar ao menu principal.\n')
             else:
                 print ('Opção digitada inválida.')
         else:
-            print ('Digite apenas números.')
+            print ('Digite apenas números.')        
     elif int(selecao_usuario) == 9:
-        #Aqui seria a opção sair, mas não fará nada por enquanto
+        #Aqui será a opção sair, mas não fará nada por enquanto
         ...
-    
     else:
         print ('Opção digitada inválida.')
-
 else:
     print ('Digite apenas números.')
