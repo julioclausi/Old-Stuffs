b49e314107acc6f35d4e6706eef84cf4b9260a36
Julio Clausi <julioclausi@gmail.com>
2023-05-10 23:31:42 -0300

    Randomize

---
+++ Code-010_random.py
@@ -0,0 +1,9 @@
+#Let's try a random function
+
+import random
+
+number = random.randrange(0,10) #from 0 to 9
+print (number)
+
+number = random.random() #from 0.000000000 to 0.99999...
+print (number)
