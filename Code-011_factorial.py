100414ef130db936efa3cd04d330765ddff20f2f
Julio Clausi <julioclausi@gmail.com>
2023-05-12 12:33:12 -0300

    Recursive factorial

---
+++ Code-011_factorial.py
@@ -0,0 +1,16 @@
+#Let's try to calculate the factorial of a integer
+
+number = input('Digit a number: ')
+factorial_number = 1
+for i in range(1,int(number)+1):
+    factorial_number = factorial_number * i
+
+print (f'{number}! = {factorial_number}')
+
+#Now using a recursive function
+def factorial(number):
+    if number == 0:
+        return 1
+    return number * factorial(number-1)
+
+print (f'Factorial of the number {number} is {factorial(int(number))}')
