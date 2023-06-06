5c79bc8bf71952434c35ca0bfc26b79e1585a8d6
Julio Clausi <julioclausi@gmail.com>
2023-06-06 01:02:27 -0300

    auto play

--- main.py
+++ main.py
@@ -17,12 +17,20 @@ def convert_the_map(list_of_strings):
             my_maze[i].append(list_of_strings[i][j])
     return my_maze
 
-#Printhing the map
+#Printing the map
 def print_the_map():
     os.system('cls')
-    for line in my_maze:
-        for row in line:
-            print(row,end='')
+    for i in range (0,len(my_maze)):
+        for j in range (0, len(my_maze[i])):
+            print (f'{my_maze[i][j]:^3}',end='')
+        print()
+
+#Printing the solution
+def print_the_solution():
+    os.system('cls')
+    for i in range (0,len(new_maze)):
+        for j in range (0, len(new_maze[i])):
+            print (f'{new_maze[i][j]:^3}',end='')
         print()
 
 #Finding the player
@@ -31,7 +39,6 @@ def find_the_player():
         for j in range (0, len(my_maze[i])):
             if my_maze[i][j] == 'X':
                 player_pos=(i,j)
-                print (player_pos)
                 return player_pos
             
 #Moving
@@ -47,20 +54,88 @@ def moving(x,y,my_maze):
         my_maze[player_pos[0]][player_pos[1]] = '.'
         player_pos = (player_pos[0]+x,player_pos[1]+y)
 
-my_maze = convert_the_map(read_the_map())
-player_pos = find_the_player()
-input('Press Enter to start')
+#Finding the end
+def find_the_end():
+    for i in range (0,len(my_maze)):
+        for j in range (0, len(my_maze[i])):
+            if my_maze[i][j] == '@':
+                end_pos=(i,j)
+                return end_pos
 
-while True:
-    print_the_map()
-    entry = input('\n W A S D? ')
-    if entry.upper() == 'W':
-        moving(-1,0,my_maze)
-    elif entry.upper() == 'A':
-        moving(0,-1,my_maze)
-    elif entry.upper() == 'S':
-        moving(1,0,my_maze)
-    elif entry.upper() == 'D':
-        moving(0,+1,my_maze)
-    elif entry.upper() == 'Q':
-        break
+def counting(i,j,counter):
+    block = new_maze[i][j]
+    if block.isdigit():
+        if int(block) > int(counter):
+            new_maze[i][j] = str(counter)
+            counting(i,j+1,counter+1)
+            counting(i+1,j,counter+1)
+            counting(i,j-1,counter+1)
+            counting(i-1,j,counter+1)
+    else:
+        if block == '@':
+            new_maze[i][j] = str(counter)
+            counting(i,j+1,counter+1)
+            counting(i+1,j,counter+1)
+            counting(i,j-1,counter+1)
+            counting(i-1,j,counter+1)
+        elif block == '.':
+            new_maze[i][j] = str(counter)
+            counting(i,j+1,counter+1)
+            counting(i+1,j,counter+1)
+            counting(i,j-1,counter+1)
+            counting(i-1,j,counter+1)
+        elif block == 'X':
+            new_maze[i][j] = str(counter)
+
+#Solving
+def solve_the_map():
+    end_pos = find_the_end()
+    counting(end_pos[0],end_pos[1],0)
+    print_the_solution()
+
+def tracing_back(x,y,counter):
+    if new_maze[x+1][y]!='#' and int(new_maze[x][y]) > int(new_maze[x+1][y]):
+        my_maze[x][y] = counter
+        tracing_back(x+1,y,counter+1)
+    elif new_maze[x][y+1]!='#' and int(new_maze[x][y]) > int(new_maze[x][y+1]):
+        my_maze[x][y] = counter
+        tracing_back(x,y+1,counter+1)
+    elif new_maze[x-1][y]!='#' and int(new_maze[x][y]) > int(new_maze[x-1][y]):
+        my_maze[x][y] = counter
+        tracing_back(x-1,y,counter+1)
+    elif new_maze[x][y-1]!='#' and int(new_maze[x][y]) > int(new_maze[x][y-1]):
+        my_maze[x][y] = counter
+        tracing_back(x,y-1,counter+1)
+
+my_maze = convert_the_map(read_the_map())
+new_maze = convert_the_map(read_the_map())
+player_pos = find_the_player()
+
+input('Press Enter to view the backtesting')
+
+solve_the_map()
+
+input('Press Enter to view the map')
+
+print_the_map()
+
+tracing_back(player_pos[0],player_pos[1],0)
+
+input('Press Enter to view the solution')
+
+print_the_map()
+
+# input('Press Enter to start')
+# while True:
+#     print_the_map()
+#     entry = input('\n W A S D? ')
+#     if entry.upper() == 'W':
+#         moving(-1,0,my_maze)
+#     elif entry.upper() == 'A':
+#         moving(0,-1,my_maze)
+#     elif entry.upper() == 'S':
+#         moving(1,0,my_maze)
+#     elif entry.upper() == 'D':
+#         moving(0,+1,my_maze)
+#     elif entry.upper() == 'Q':
+#         break
