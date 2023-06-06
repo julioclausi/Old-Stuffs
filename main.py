152a15f84cef90b7a59a5b53df50e1bdb71da475
Julio Clausi <julioclausi@gmail.com>
2023-06-05 23:50:19 -0300

    playable

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import os
+from pynput import keyboard
+from pynput.keyboard import Key
+
 #Reading the map
 def read_the_map():
     with open('maze.txt') as tex:
@@ -14,12 +18,49 @@ def convert_the_map(list_of_strings):
     return my_maze
 
 #Printhing the map
-def print_the_map(my_maze):
+def print_the_map():
+    os.system('cls')
     for line in my_maze:
         for row in line:
             print(row,end='')
         print()
 
-my_maze = convert_the_map(read_the_map())
+#Finding the player
+def find_the_player():
+    for i in range (0,len(my_maze)):
+        for j in range (0, len(my_maze[i])):
+            if my_maze[i][j] == 'X':
+                player_pos=(i,j)
+                print (player_pos)
+                return player_pos
+            
+#Moving
+def moving(x,y,my_maze):
+    global player_pos 
+    if my_maze[player_pos[0]+x][player_pos[1]+y] == '#':
+        ...
+    elif my_maze[player_pos[0]+x][player_pos[1]+y] == '@':
+        print ("YOU WIN!!!")
+        quit()
+    else:
+        my_maze[player_pos[0]+x][player_pos[1]+y] = 'X'
+        my_maze[player_pos[0]][player_pos[1]] = '.'
+        player_pos = (player_pos[0]+x,player_pos[1]+y)
 
-print_the_map (my_maze)
+my_maze = convert_the_map(read_the_map())
+player_pos = find_the_player()
+input('Press Enter to start')
+
+while True:
+    print_the_map()
+    entry = input('\n W A S D? ')
+    if entry.upper() == 'W':
+        moving(-1,0,my_maze)
+    elif entry.upper() == 'A':
+        moving(0,-1,my_maze)
+    elif entry.upper() == 'S':
+        moving(1,0,my_maze)
+    elif entry.upper() == 'D':
+        moving(0,+1,my_maze)
+    elif entry.upper() == 'Q':
+        break
