0664f9bb118f2f2179761445a097413892c0ff44
Julio Clausi <julioclausi@gmail.com>
2024-03-11 20:49:54 -0300

    Learning Linked Lists

---
+++ 02_LinkedLists.py
@@ -0,0 +1,11 @@
+# Learning Linked Lists
+
+# Properties:
+# 1. List of items that can be spread out in memory and connected by pointers
+# 2. The size of the list can increase or decrease during the execution of the program
+
+# ______      ______      ______
+# |data|      |data|      |data|
+# |next| ---> |next| ---> |next| ---> None
+# ‾‾‾‾‾‾      ‾‾‾‾‾‾      ‾‾‾‾‾‾
+
