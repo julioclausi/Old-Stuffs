ed50c564982fee62ebeb8c5db6ec23bb5e9fc882
Julio Clausi <julioclausi@gmail.com>
2024-03-11 18:37:15 -0300

    Learning Arrays

---
+++ 00_Arrays.py
@@ -0,0 +1,33 @@
+# Learning the concept about Arrays
+
+# The id() function returns a unique id for the specified object
+# https://docs.python.org/3/library/functions.html#id
+
+MAX_LENGHT = 5
+
+array = [None] * MAX_LENGHT
+
+for i in range(0,MAX_LENGHT):
+    array[i] = i
+    print (id(array[i]))
+
+# The result was:
+# 140731377668872
+# 140731377668904
+# 140731377668936
+# 140731377668968
+# 140731377669000
+
+# Conclusion:
+# An array is a sequential list of data
+# Each element is stored right after the previous one in memory
+# Arrays are very fast since each element follows on from the previous one
+# But if your array is really big and you are low in memory it could be impossible to find large enough storage to fit your entire array
+
+# https://docs.python.org/3/library/exceptions.html#MemoryError
+# s = []
+# for i in range(1000):
+#    for j in range(1000):
+#        for k in range(1000):
+#            s.append("More")
+# Exit: MemoryError
