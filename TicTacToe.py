daff89843d826b30ce1d29bcd9b0305520f68906
Julio Clausi <julioclausi@gmail.com>
2024-03-25 16:55:43 -0300

    studying minimax

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,70 +1,86 @@
-class Jogo:
+class Jogo ():
     def __init__ (self):
-        self.tabuleiro = [' '] * 10
+        self.tabuleiro = [' '] * 9
 
     def show (self):
-        for i in range (0,3):
-            print (f'{self.tabuleiro[1+(i*3)]}|{self.tabuleiro[2+(i*3)]}|{self.tabuleiro[3+(i*3)]}')
-            if i == 2:
-                return
-            print ('------')
+        print (f'{self.tabuleiro[0]}|{self.tabuleiro[1]}|{self.tabuleiro[2]}')
+        print (f'-----')
+        print (f'{self.tabuleiro[3]}|{self.tabuleiro[4]}|{self.tabuleiro[5]}')
+        print (f'-----')
+        print (f'{self.tabuleiro[6]}|{self.tabuleiro[7]}|{self.tabuleiro[8]}')
 
-    def jogadaspossiveis (self):
-        possiveis = []
-        for i in range (1,10):
-            if self.tabuleiro[i] == 'X' or self.tabuleiro[i] == 'O':
-                ...
-            else:
-                possiveis.append(i)
-        print (f'Jogadas possiveis: {possiveis}')
-        if len(possiveis) < 1:
-            return True
-        return False
+    def jogadas_possiveis (self):
+        jogadas = []
+        for i in range (0,9):
+            if self.tabuleiro[i] == ' ':
+                jogadas.append(i)
+        return jogadas
 
-    def joga (self, jogador):
-        self.jogadaspossiveis()
-        jogada = int (input ('Escolha uma: '))
-        self.tabuleiro[jogada] = jogador
-
-    def verificavitoria (self):
-        if self.tabuleiro[1] == self.tabuleiro[2] and self.tabuleiro[1] == self.tabuleiro[3] and self.tabuleiro[1] != ' ':
+    def vitoria (self):
+        if self.tabuleiro[0] == self.tabuleiro[1] and self.tabuleiro[0] == self.tabuleiro[2] and self.tabuleiro[0] != ' ':
             return True
-        elif self.tabuleiro[4] == self.tabuleiro[5] and self.tabuleiro[4] == self.tabuleiro[6] and self.tabuleiro[4] != ' ':
+        elif self.tabuleiro[3] == self.tabuleiro[4] and self.tabuleiro[3] == self.tabuleiro[5] and self.tabuleiro[3] != ' ':
             return True
-        elif self.tabuleiro[7] == self.tabuleiro[8] and self.tabuleiro[7] == self.tabuleiro[9] and self.tabuleiro[7] != ' ':
+        elif self.tabuleiro[6] == self.tabuleiro[7] and self.tabuleiro[6] == self.tabuleiro[8] and self.tabuleiro[6] != ' ':
+            return True
+        elif self.tabuleiro[0] == self.tabuleiro[3] and self.tabuleiro[0] == self.tabuleiro[6] and self.tabuleiro[0] != ' ':
             return True
         elif self.tabuleiro[1] == self.tabuleiro[4] and self.tabuleiro[1] == self.tabuleiro[7] and self.tabuleiro[1] != ' ':
             return True
         elif self.tabuleiro[2] == self.tabuleiro[5] and self.tabuleiro[2] == self.tabuleiro[8] and self.tabuleiro[2] != ' ':
             return True
-        elif self.tabuleiro[3] == self.tabuleiro[6] and self.tabuleiro[3] == self.tabuleiro[9] and self.tabuleiro[3] != ' ':
+        elif self.tabuleiro[0] == self.tabuleiro[4] and self.tabuleiro[0] == self.tabuleiro[8] and self.tabuleiro[0] != ' ':
             return True
-        elif self.tabuleiro[1] == self.tabuleiro[5] and self.tabuleiro[1] == self.tabuleiro[9] and self.tabuleiro[1] != ' ':
+        elif self.tabuleiro[2] == self.tabuleiro[4] and self.tabuleiro[2] == self.tabuleiro[6] and self.tabuleiro[2] != ' ':
             return True
-        elif self.tabuleiro[3] == self.tabuleiro[5] and self.tabuleiro[3] == self.tabuleiro[7] and self.tabuleiro[3] != ' ':
-            return True
+
+    def jogada (self,player):
+        if player == 1:
+            # XXXXX
+            print (self.jogadas_possiveis())
+            play = int (input ('Jogador X entre com uma opção acima: '))
+            self.tabuleiro[play] = 'X'
         else:
-            return False
+            # OOOOO
+            print (self.jogadas_possiveis())
+            play = int (input ('Jogador O entre com uma opção acima: '))
+            self.tabuleiro[play] = 'O'
 
-jogo = Jogo()
+    def teste1 (self):
+        """
+        X|0|X
+         |0|X
+        X| |0
+        """
+        self.tabuleiro[0] = 'X'
+        self.tabuleiro[2] = 'X'
+        self.tabuleiro[5] = 'X'
+        self.tabuleiro[6] = 'X'
+        self.tabuleiro[1] = 'O'
+        self.tabuleiro[4] = 'O'
+        self.tabuleiro[8] = 'O'
 
-jogador = 'X'
-jogo.show()
+
+
+
+
+player = -1
+novo_jogo = Jogo()
+novo_jogo.teste1()
 
 while True:
-    jogo.joga(jogador)
-    jogo.show()
-    if jogo.verificavitoria() or jogo.jogadaspossiveis():
+    novo_jogo.show()
+    novo_jogo.jogada(player)
+    novo_jogo.show()
+    if novo_jogo.vitoria():
+        print ('Vitoria')
+        if player == 1:
+            print ('Jogador X venceu')
+        else:
+            print ('Jogador O venceu')
         break
-    if jogador == 'X':
-        jogador = 'O'
     else:
-        jogador = 'X'
-
-if jogo.verificavitoria():
-    if jogador == 'X':
-        print ('X VENCEU')
-    if jogador == 'O':
-        print ('O VENCEU')
-else:
-    print ('EMPATE!')
+        if len(novo_jogo.jogadas_possiveis()) <1:
+            print ('Empate')
+            break
+    player *= -1
