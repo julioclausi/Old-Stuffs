b42714040acca8e1f656f7bba1356fed9df52fdd
Julio Clausi <julioclausi@gmail.com>
2024-03-25 19:16:11 -0300

    minmax works

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,7 @@
 class Jogo ():
     def __init__ (self):
         self.tabuleiro = [' '] * 9
+        self.turno = 0
 
     def show (self):
         print (f'{self.tabuleiro[0]}|{self.tabuleiro[1]}|{self.tabuleiro[2]}')
@@ -43,8 +44,9 @@ class Jogo ():
         else:
             # OOOOO
             print (self.jogadas_possiveis())
-            play = int (input ('Jogador O entre com uma opção acima: '))
-            self.tabuleiro[play] = 'O'
+            print ('Jogada do computador')
+            jogada = melhor_jogada(self)
+            self.tabuleiro[jogada] = 'O'
 
     def teste1 (self):
         """
@@ -60,27 +62,84 @@ class Jogo ():
         self.tabuleiro[4] = 'O'
         self.tabuleiro[8] = 'O'
 
+    def empate (self):
+        jogadas = self.jogadas_possiveis()
+        if len(jogadas)<1:
+            return True
+        return False
 
+    def calcula (self,player):
+        if self.vitoria():
+            if self.turno == player:
+                return -1
+            else:
+                return 1
+        else:
+            return 0
 
+def minimax (jogo_teste: Jogo,player,maximizing):
 
+    if jogo_teste.vitoria() or jogo_teste.empate():
+        return jogo_teste.calcula(player)
+    
+    if maximizing:
+        melhor_valor = -10
+        jogadas = jogo_teste.jogadas_possiveis()
+        for jogada in jogadas:
+            if player == 1:
+                jogo_teste.tabuleiro[jogada] = 'X'
+            else:
+                jogo_teste.tabuleiro[jogada] = 'O'
+            resultado = minimax(jogo_teste,player*-1,False)
+            jogo_teste.tabuleiro[jogada] = ' '
+            melhor_valor = max(melhor_valor,resultado)
+        return melhor_valor
 
-player = -1
-novo_jogo = Jogo()
-novo_jogo.teste1()
+    else:
+        pior_valor = 10
+        jogadas = jogo_teste.jogadas_possiveis()
+        for jogada in jogadas:
+            if player == 1:
+                jogo_teste.tabuleiro[jogada] = 'X'
+            else:
+                jogo_teste.tabuleiro[jogada] = 'O'
+            resultado = minimax(jogo_teste,player*-1,True)
+            jogo_teste.tabuleiro[jogada] = ' '
+            pior_valor = min (pior_valor,resultado)
+        return pior_valor
+
+def melhor_jogada (jogo_teste: Jogo):
+    jogada_correta = -1
+    melhor_resultado = -10
+    jogadas = jogo_teste.jogadas_possiveis()
+    for jogada in jogadas:
+        if jogo_teste.turno == 1:
+            jogo_teste.tabuleiro[jogada] = 'X'
+        else:
+            jogo_teste.tabuleiro[jogada] = 'O'
+        resultado = minimax(jogo_teste,jogo_teste.turno*-1,False)
+        jogo_teste.tabuleiro[jogada] = ' '
+        if resultado > melhor_resultado:
+            melhor_resultado = resultado
+            jogada_correta = jogada
+    return jogada_correta
+
+jogo = Jogo()
+# jogo.teste1()
+jogo.show()
+jogo.turno = 1
 
 while True:
-    novo_jogo.show()
-    novo_jogo.jogada(player)
-    novo_jogo.show()
-    if novo_jogo.vitoria():
-        print ('Vitoria')
-        if player == 1:
-            print ('Jogador X venceu')
+    jogo.jogada(jogo.turno)
+    jogo.show()
+    if jogo.vitoria():
+        if jogo.turno == -1:
+            print ('Computador venceu')
         else:
-            print ('Jogador O venceu')
+            print ('Jogador venceu')
         break
     else:
-        if len(novo_jogo.jogadas_possiveis()) <1:
+        if jogo.empate():
             print ('Empate')
             break
-    player *= -1
+    jogo.turno *= -1
