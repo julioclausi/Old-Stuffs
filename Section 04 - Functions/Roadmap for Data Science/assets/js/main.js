b62cc2445144a3e9a06ecb948a90370f8b1b8d72
Julio Clausi <julioclausi@gmail.com>
2024-12-19 16:29:44 -0300

    roadmap for data science

---
+++ Section 04 - Functions/Roadmap for Data Science/assets/js/main.js
@@ -0,0 +1,36 @@
+const coll = document.getElementsByClassName("collapsible");
+const itens = document.getElementsByClassName('item');
+const contents = document.getElementsByClassName('content');
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        //this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+for (let i = 0; i < itens.length; i++) {
+    itens[i].addEventListener('click', function () {
+        for (item of itens) {
+            item.classList.remove("active");
+        }
+        this.classList.toggle("active");
+        for (let j = 0; j<contents.length;j++){
+            if (i !== j) {
+                let content = contents[j];
+                content.style.display = "none";
+            }
+        }
+        let content = contents[i];
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
