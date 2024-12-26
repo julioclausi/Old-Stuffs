7fba199aecba3540498d435ed5befe9616d4f0c5
Julio Clausi <julioclausi@gmail.com>
2024-12-26 17:57:44 -0300

    constructor function com metodos

--- Section 04 - Functions/constructorfunc.js
+++ Section 04 - Functions/constructorfunc.js
@@ -2,9 +2,22 @@ function Pessoa(nome, sobrenome) {
     this.nome = nome;
     this.sobrenome = sobrenome;
     // não precisa do return this;
+
+    const metodoInterno = function() {
+        console.log('interno..');
+    }
+
+    this.metodo = function() {
+        console.log(this.nome + ': olá!');
+        metodoInterno();
+    }
 }
+
+//com this é publico
+//sem this é privado
 
 const pessoa = new Pessoa('Julio', 'Roberto');
 console.log(pessoa);
 console.log(pessoa.nome);
 console.log(pessoa.sobrenome);
+pessoa.metodo();
