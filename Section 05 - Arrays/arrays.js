d1c188825a2019c2f123f7c8a93a8e1df52476c8
Julio Clausi <julioclausi@gmail.com>
2025-01-02 12:13:46 -0300

    arrays review

--- Section 05 - Arrays/arrays.js
+++ Section 05 - Arrays/arrays.js
@@ -1,4 +1,4 @@
-const nomes = ['A','B','C'];
+const nomes = ['Abacaxi','Banana','Carambola'];
 
 //valor por referência
 const outrosNomes = nomes;
@@ -6,11 +6,23 @@ const outrosNomes = nomes;
 //cópia
 const maisNomes = [...nomes];
 
-outrosNomes.pop();
+const removidoDoFinal = outrosNomes.pop(); //método
+const removidoDoComeco = outrosNomes.shift();
 
-maisNomes.push('D');
+maisNomes.push('Damasco'); //adiciona no final
+maisNomes.unshift('Abacate'); //adiciona no começo
 
 console.log(nomes);
 console.log(outrosNomes);
 console.log(maisNomes);
-console.log(maisNomes.length);
+console.log(maisNomes.length); //atributo
+console.log(removidoDoComeco);
+console.log(removidoDoFinal);
+
+const frase = 'Era uma vez';
+const letras = [...frase];
+const palavras = frase.split(' ');
+console.log(letras);
+console.log(palavras);
+const outraFrase = palavras.join(' ');
+console.log(outraFrase);
