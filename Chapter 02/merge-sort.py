8765b2561c5bbc84e87208c273539697c9f34e09
Julio Clausi <julioclausi@gmail.com>
2025-01-08 13:14:28 -0300

    merge sort v2

--- Chapter 02/merge-sort.py
+++ Chapter 02/merge-sort.py
@@ -1,3 +1,14 @@
+import os
+from time import sleep
+import random
+
+def printing(myList):
+    sleep(0.5)
+    os.system('cls')
+    print(myList)
+    for number in myList:
+        print(f'{number*"-":^30}')
+
 def merge(A,inicio,fim):
     meio = int((inicio + fim) / 2)
     esquerda = A[inicio:meio+1]
@@ -13,6 +24,8 @@ def merge(A,inicio,fim):
             A[k] = direita[j]
             j+=1
         k+=1
+        printing(A)
+
     while i<len(esquerda):
         A[k] = esquerda[i]
         k+=1
@@ -30,7 +43,8 @@ def mergeSort(A,inicio,fim):
     mergeSort(A,meio+1,fim)
     merge(A,inicio,fim)
 
-array = [6,5,7,4,1,8,3,2]
-print(array)
-mergeSort(array,0,len(array)-1)
-print(array)
+myList = [i for i in range(1,30,2)]
+random.shuffle(myList)
+print(myList)
+mergeSort(myList,0,len(myList)-1)
+print(myList)
