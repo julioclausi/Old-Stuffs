b6c3d40a604ad212f34cba368ef5f5d1482f8534
Julio Clausi <julioclausi@gmail.com>
2025-01-10 17:33:58 -0300

    heap sort page 120

--- Chapter 06/heap-sort.py
+++ Chapter 06/heap-sort.py
@@ -7,17 +7,16 @@ def direita (i):
 def pai (i):
     return int((i-1)/2)
 
-def constroiMaxHeap(A):
-    primeiraFolha = int(len(A)/2)
+def constroiMaxHeap(A,tamanho):
+    primeiraFolha = int(tamanho/2)
     ultimoNodo = primeiraFolha-1
     for i in range(ultimoNodo, -1, -1):
-        print(i)
-        maximizaHeap(A,i)
+        maximizaHeap(A,i,tamanho)
 
-def maximizaHeap (A,i):
+def maximizaHeap (A,i,tamanho):
     esq = esquerda(i)
     dir = direita(i)
-    tam = len(A)
+    tam = tamanho
     if (esq < tam) and (A[esq] > A[i]):
         maior = esq
     else:
@@ -26,10 +25,20 @@ def maximizaHeap (A,i):
         maior = dir
     if maior != i:
         A[maior], A[i] = A[i], A[maior]
-        maximizaHeap(A,maior)
+        maximizaHeap(A,maior,tamanho)
+
+def heapSort (A):
+    qtdDeItens = len(A)
+    constroiMaxHeap(A, qtdDeItens) # O(n)
+    for i in range(qtdDeItens-1,0,-1):
+        A[i], A[0] = A[0], A[i]
+        qtdDeItens-=1
+        maximizaHeap(A,0,qtdDeItens) # O(lgn)
+        # portanto será O(n lgn)
 
 # myArray = [16,4,10,14,7,9,3,2,8,1]
-myArray = [4,1,3,2,16,9,10,14,8,7]
+# myArray = [4,1,3,2,16,9,10,14,8,7]
+myArray = [9,8,7,6,5,4,3,2,1]
 print(myArray)
-constroiMaxHeap(myArray)
+heapSort(myArray)
 print(myArray)
