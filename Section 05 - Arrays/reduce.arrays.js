1ab6c3005df3224be971b6ef6f9ce017119f1d7e
Julio Clausi <julioclausi@gmail.com>
2025-01-15 16:29:45 -0300

    reduce arrays v2

--- Section 05 - Arrays/reduce.arrays.js
+++ Section 05 - Arrays/reduce.arrays.js
@@ -6,3 +6,31 @@ const somaTotal = numeros.reduce(
     }, 0);
 
 console.log(somaTotal);
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduardo', idade: 55 },
+    { nome: 'Wallace', idade: 47 }
+]
+
+const maisVelho = pessoas.reduce(function (velho, valor) {
+    if (velho === 0) {
+        velho = valor;
+    } else {
+        if (velho.idade < valor.idade) {
+            velho = valor;
+        }
+    }
+    return velho;
+}, 0);
+
+const maisNovo = pessoas.reduce(function (novo, valor) {
+    if (valor.idade < novo.idade) return valor;
+    return novo;
+});
+
+console.log(maisVelho);
+console.log(maisNovo);
+
+//Da pra fazer map e filter com reduce!
