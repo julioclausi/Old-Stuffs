22f7ee0c4c9d2712083759c372f7f117916c57d0
Julio Clausi <julioclausi@gmail.com>
2025-01-20 18:34:57 -0300

    race all

--- Section 08 - JS Asynchronous/01 - promises.js
+++ Section 08 - JS Asynchronous/01 - promises.js
@@ -19,24 +19,70 @@ function aleatorio(min, max) {
 
 function esperaAi(msg, tempo) {
     return new Promise((resolve, reject) => {
-        if(typeof msg!== 'string') reject('Bad value');
         setTimeout(() => {
-            resolve(msg);
+            if (typeof msg !== 'string') {
+                reject('Bad value');
+                return;
+            }
+            resolve(msg.toUpperCase() + ' - passei na promise');
         }, tempo);
     });
 }
 
-esperaAi('[1] Conectando ao BD', aleatorio(1, 3))
-    .then(mensagem => {
-        console.log(mensagem);
-        return esperaAi('[2] Buscando dados do BD', aleatorio(1, 3));
-    }).then(mensagem => {
-        console.log(mensagem);
-        return esperaAi('[3] Tratando os dados', aleatorio(1, 3))
-    }).then(mensagem => {
-        console.log(mensagem);
-    }).then(() => {
-        console.log('[4] Exibindo os dados');
-    }).catch(e => {
-        console.log(e);
-    });
+// esperaAi('[1] Conectando ao BD', aleatorio(1, 3))
+//     .then(mensagem => {
+//         console.log(mensagem);
+//         return esperaAi('[2] Buscando dados do BD', aleatorio(1, 3));
+//     }).then(mensagem => {
+//         console.log(mensagem);
+//         return esperaAi('[3] Tratando os dados', aleatorio(1, 3))
+//     }).then(mensagem => {
+//         console.log(mensagem);
+//     }).then(() => {
+//         console.log('[4] Exibindo os dados');
+//     }).catch(e => {
+//         console.log(e);
+//     });
+
+// const promises = [
+//     //'Primeiro valor',
+//     esperaAi('Promise 1', 2000),
+//     esperaAi('Promise 2', 500),
+//     esperaAi('Promise 3', 1000),
+//     esperaAi(5, 5000),
+//     //'Último valor'
+// ];
+
+// Promise.all(promises)
+//     .then(function (valor) {
+//         console.log('Retorna uma lista com tudo pronto:', valor);
+//     })
+//     .catch(e => {
+//         console.log('Caiu no erro e não retorna nenhuma');
+//     }
+//     );
+
+// entrega o valor da primeira que resolver, mas faz todas
+// Promise.race(promises)
+//     .then(function (valor) {
+//         console.log('Retorna a mais rápida:', valor);
+//     })
+//     .catch(e => {
+//         console.log('Caiu no erro e não retorna nenhuma');
+//     }
+//     );
+
+function baixaPagina() {
+    const emCache = false;
+    if (emCache) {
+        return Promise.resolve('Página em cache');
+    } else {
+        return esperaAi('Baixei a página', 3000);
+    }
+}
+
+baixaPagina()
+    .then(dadosPagina => {
+        console.log(dadosPagina);
+    })
+    .catch(e => console.log('ERRO', e));
