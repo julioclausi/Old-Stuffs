27f934a2bdbe5aa643f63d5f1e755712546b89df
Julio Clausi <julioclausi@gmail.com>
2025-01-22 13:46:43 -0300

    path __dirname

--- Section 10 - Node/01 - Modules/app.js
+++ Section 10 - Node/01 - Modules/app.js
@@ -3,6 +3,11 @@ const fala = mod.falaNome;
 
 console.log(mod.nome);
 console.log(fala());
+console.log(__filename);
+console.log(__dirname);
+
+const path = require('path');
+console.log(path.resolve(__dirname, '..', '..', 'modelo'));
 
 // npm init -y
 // npm u axios
